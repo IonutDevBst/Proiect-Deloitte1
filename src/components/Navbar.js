68e1c128b58c25a1fcad18e6a39b4a1e7e5b039b
andreeadev <94364524+andreeadev@users.noreply.github.com>
2021-11-16 09:59:10 +0200

    Updated donut component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlin
 import { ReactComponent as Logo } from "./resources/Group 1327.svg";
 
 const Navbar = () => {
+<<<<<<< Updated upstream
   const [isMobile, setIsMobile] = useState(true);
 
   return (
@@ -60,3 +61,54 @@ const Navbar = () => {
 };
 
 export default Navbar;
+=======
+
+    const [isMobile, setIsMobile] = useState(true);
+
+    return (
+      
+        <nav className="navbar">
+           <ul className="nav-links navbar">
+               <Link to="/home" className="home">
+                   <li className="novacolor">Gelato &amp; Donuts</li>
+               </Link>
+               <Link to="/gelato" className="gelato">
+                   <li className="novacolor">Gelato</li>
+               </Link>
+               <Link to="/gelatoforspecialneeds" className="gelatoforspecialneeds">
+                   <li className="novacolor">GELATO FOR SPECIAL NEEDS </li>
+               </Link>
+               <Link to="/donuts" className="donuts">
+                   <li className="novacolor">DONUTS</li>
+               </Link>
+               <Link to="/donutsforspecialneeds" className="donutsforspecialneeds">
+                   <li className="novacolor">DONUTS FOR SPECIAL NEEDS</li>
+               </Link>
+             
+
+
+           </ul>
+            <ul className="nav-links navbar">
+            <Link to="/account" className="account">
+                    <li className="novacolor account1"><AccountCircleOutlinedIcon />Account</li>
+                    <li className="novacolor"> Account</li> 
+
+               </Link>
+               <Link to="/basket" className="basket">
+                   <li className=" novacolor account1"><ShoppingBasketOutlinedIcon />Basket</li>
+                   <li className="novacolor account1">Basket</li>
+               </Link>
+
+            </ul>
+
+
+           <button className="mobile-menu-icon">
+               {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i> }
+           </button>
+        </nav> 
+       
+    )
+}
+
+export default Navbar
+>>>>>>> Stashed changes
