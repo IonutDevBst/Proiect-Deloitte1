b06cd63dd7f444f41ec1db351be6759a3332498f
mharalamb <94360271+mharalamb@users.noreply.github.com>
2021-11-16 10:44:31 +0200

    Homepage partial

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import image from "./resoruces2/Group 1443.png";
+import "./Home.css";
 
 const Home = () => {
-    return <div>Welcome to my page! Marian :)</div>;
-}
+  return (
+    <div className="card-allign">
+      <div className="text-to-image">
+        <img src={image} className="imghome "></img>
+        <div className="centered">DONUTS</div>
+      </div>
+      <div className="text-to-image">
+        <img src={image} className="imghome "></img>
+        <div className="centered">DONUTS</div>
+      </div>
+      <div className="text-to-image">
+        <img src={image} className="imghome "></img>
+        <div className="centered">DONUTS</div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
