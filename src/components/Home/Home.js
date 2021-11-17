3a6aafb9cc1885214077d18caa3c516fe0028ec1
andreeadev <94364524+andreeadev@users.noreply.github.com>
2021-11-17 17:16:07 +0200

    Added functionality on explore buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,15 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActions } from "@mui/material";
+import { Button, CardActions} from "@mui/material";
+import { Link } from 'react-router-dom';
 import Cards from "../Cards/Cards";
 import { ReactComponent as NextButton } from "../resoruces2/Group 1340.svg";
 import { useState, useEffect } from "react";
 import { getListProducts } from "../../api";
 import image2 from "../resoruces2/Group 1444.png";
 import image3 from "../resoruces2/Group 1445.png";
+import Gelato from "../Gelato/Gelato";
 
 const Home = (props) => {
   const [product, setProduct] = useState([]);
@@ -32,7 +34,7 @@ const Home = (props) => {
             ICE CREAM &amp; GELATO
           </Typography>
           <CardActions className="font-button">
-            <Button className="font-button1">Explore options</Button>
+            <Button component={Link} to="../Gelato"  className="font-button1">Explore options</Button>
           </CardActions>
         </Card>
         <Card sx={{ width: 590 }} className="root">
@@ -41,7 +43,7 @@ const Home = (props) => {
             DELICIOUS DONUTS
           </Typography>
           <CardActions className="font-button">
-            <Button className="font-button1">Explore options</Button>
+            <Button component={Link} to="../Donuts" className="font-button1">Explore options</Button>
           </CardActions>
         </Card>
         <Card sx={{ width: 590 }} className="root">
@@ -50,7 +52,7 @@ const Home = (props) => {
             FAMILY / CORPORATE
           </Typography>
           <CardActions className="font-button">
-            <Button className="font-button1">Explore options</Button>
+            <Button component={Link} to="../DonutsForSpecialNeeds" className="font-button1">Explore options</Button>
           </CardActions>
         </Card>
       </div>
@@ -58,7 +60,7 @@ const Home = (props) => {
         <Button disabled>
           <h2 className="title-icecream">TOP SELLING ICE CREAM &amp; GELATO</h2>
         </Button>
-        <Button className="btn-icon">
+        <Button component={Link} to="../Gelato" className="btn-icon">
           <h2 className="button-next">EXPLORE ALL </h2>
           <NextButton />
         </Button>
@@ -80,7 +82,7 @@ const Home = (props) => {
         <Button disabled>
           <h2 className="title-icecream">TOP SELLING DONUTS </h2>
         </Button>
-        <Button className="btn-icon">
+        <Button component={Link} to="../Donuts" className="btn-icon">
           <h2 className="button-next">EXPLORE ALL </h2>
           <NextButton />
         </Button>
